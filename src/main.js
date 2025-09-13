37fbf8ad167be6d665c89bce406d60613c977a77
Nathan <dsouzanathan09@gmail.com>
2025-09-13 16:16:19 +0400

    mistake

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.module.js';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
